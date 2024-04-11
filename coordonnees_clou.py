64afd563da0c05da1495b7590d5ff610739cbd41
LucasParisienG <163561916+LucasParisienG@users.noreply.github.com>
2024-04-11 15:08:44 -0400

    Update coordonnees_clou.py

--- coordonnees_clou.py
+++ coordonnees_clou.py
@@ -5,16 +5,15 @@ from math import sin,cos,pi,tan
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-"""
-Cette fonction applique une réflexion miroir à un point par rapport à un axe spécifié  
 
-Arguments:
-    point (tuple) : représent les coordonnées du points à réfléchir 
-    axe (str) : représent l'axe de réflexion 'x' ou 'y'. Si l'axe choisie est x, il y aura réflexion par rapport à l'axe verticale (miroir horizontal), : changement de la coordonnées y
-        si l'axe 'y' est choisie il y aura reflexion par rapport à l'axe horizontal(miroir vertical) : changement de la coordonnées x   
-Retourne :
-    (tuple) : Représent les nouvelles coordonnées du point refletté 
-"""
+# Cette fonction applique une réflexion miroir à un point par rapport à un axe spécifié  
+
+# Arguments:
+    # point (tuple) : représent les coordonnées du points à réfléchir 
+    # axe (str) : représent l'axe de réflexion 'x' ou 'y'. Si l'axe choisie est x, il y aura réflexion par rapport à l'axe verticale (miroir horizontal), : changement de la coordonnées y
+    # si l'axe 'y' est choisie il y aura reflexion par rapport à l'axe horizontal(miroir vertical) : changement de la coordonnées x   
+# Retourne :
+    #(tuple) : Représent les nouvelles coordonnées du point refletté 
 
 
 def calculer_reflexion_point(point, axe):
@@ -34,17 +33,17 @@ def calculer_reflexion_point(point, axe):
     return (a, b)  # TUPLE REFLETTÉ
 
 
-"""
-Cette fonction fait tourner un point dans le plan cartésien, autour d'un point centrale donné d'un certain angle (!!attention la varaition du centre n'a pas été testé mais à été pris en considération)
 
-Arguments:
-    point (tuple) : représent les coordonnées du points à faire pivoter 
-    angle (float) : représent l'angle de rotation en degrés. une valeur positve provoque une rotation antihoraire 
-        une valeur negative provoque une rotation horaire 
-    center( tuple) : représente les coordonnées du centre de rotation (par défaut: (0,0))
-Retourne :
-    (tuple) : Représent les nouvelles coordonnées du point après rotation. (arrondie à deux chiffres après la virgule) 
-"""
+# Cette fonction fait tourner un point dans le plan cartésien, autour d'un point centrale donné d'un certain angle (!!attention la varaition du centre n'a pas été testé mais à été pris en considération)
+
+# Arguments:
+    #point (tuple) : représent les coordonnées du points à faire pivoter 
+    #angle (float) : représent l'angle de rotation en degrés. une valeur positve provoque une rotation antihoraire 
+    #une valeur negative provoque une rotation horaire 
+    #center( tuple) : représente les coordonnées du centre de rotation (par défaut: (0,0))
+# Retourne :
+    # (tuple) : Représent les nouvelles coordonnées du point après rotation. (arrondie à deux chiffres après la virgule) 
+
 def calculer_rotate_point(point,angle,center):
         rad = angle * (pi / 180)
 
@@ -58,19 +57,18 @@ def calculer_rotate_point(point,angle,center):
 
 
 
-'''
-Cette fonction applique une inclinaison (cisaillement) sur un point. L'inclinaison 
-est déterminée par un angle qui peut être en 'x' ou en 'y'.
 
-Arguments:
-    point (tuple): représente les coordonnées du point à incliner (x,y) 
-    angle (float): représente l'intensité de l'angle d'inclinaison en degrés.
-    direction (str): représent la direction de l'inclinaison, pour un inclinaison horizontale utliser 'x' et pour un inclinaison verticale utiliser 'y'
+# Cette fonction applique une inclinaison (cisaillement) sur un point. L'inclinaison 
+# est déterminée par un angle qui peut être en 'x' ou en 'y'.
 
-Retourne:
-    tuple : valeur de coordonnée après inclinaison. (Arrondie à deux chiffres après la virgule)
+# Arguments:
+    # point (tuple): représente les coordonnées du point à incliner (x,y) 
+    # angle (float): représente l'intensité de l'angle d'inclinaison en degrés.
+    # direction (str): représent la direction de l'inclinaison, pour un inclinaison horizontale utliser 'x' et pour un inclinaison verticale utiliser 'y'
 
-'''
+# Retourne:
+    # tuple : valeur de coordonnée après inclinaison. (Arrondie à deux chiffres après la virgule)
+
 
 def calculer_inclinaison_point(point,angle,direction):
     x = float(point[0])
@@ -89,21 +87,19 @@ def calculer_inclinaison_point(point,angle,direction):
 
 
 
-'''
 
-
-SECTION 2 : Calcule et transformation géométrique des coordonnées d'un Clou
+# SECTION 2 : Calcule et transformation géométrique des coordonnées d'un Clou
 
 
 
-Cette fonction détermine les coordonnées d'un clou en suivant la paramétrisation dans la Figure 1
+# Cette fonction détermine les coordonnées d'un clou en suivant la paramétrisation dans la Figure 1
 
-Arguments : 
-    a,b,c,d,e (float) : représente les dimensions spécifiques du clou, utilisé pour calculer les coordonnées.
-Retourne : 
-    list : Une liste de tuple de laquelle chaque tuples contient : une Chaine de caractères ( nom du point) et un tuple de deux nombres (float, float)
+#Arguments : 
+    #a,b,c,d,e (float) : représente les dimensions spécifiques du clou, utilisé pour calculer les coordonnées.
+#Retourne : 
+    #list : Une liste de tuple de laquelle chaque tuples contient : une Chaine de caractères ( nom du point) et un tuple de deux nombres (float, float)
         représentant les coordonnées du point dans un plan 2D
-'''
+
 
 def calculer_coordonnees_clou(a,b,c,d,e):
 
