198b2b66a3e32048c9358f7332addde4c5ac0ce0
LucasParisienG <163561916+LucasParisienG@users.noreply.github.com>
2024-04-12 17:52:15 -0400

    Update test_manipulation_histogramme.py

--- test_manipulation_histogramme.py
+++ test_manipulation_histogramme.py
@@ -3,13 +3,13 @@ import unittest
 import numpy as np
 
 def test_calculer_histogramme():
-    assert mh.calculer_histogramme(np.array([225, 160, 10, 49, 20, 170, 1, 121, 30, 223, 230, 100, 255, 23, 155, 88]), 3) == (4, 0, 2, 3, 3, 2, 2, 2, 4, 0, 2, 3, 2, 3, 2, 2)
+    assert mh.calculer_histogramme(np.array([225, 160, 10, 49, 20, 170, 1, 121, 30, 223, 230, 100, 255, 23, 155, 88]), 3) == np.array([4, 0, 2, 3, 3, 2, 2, 2, 4, 0, 2, 3, 2, 3, 2, 2])
     
 def test_calculer_distance_1():
-    assert mh.calculer_distance_1((1, 2, 3, 4, 5), (2, 3, 4, 5, 6)) == 2.24
+    assert mh.calculer_distance_1(np.array([1, 2, 3, 4, 5]), np.array([2, 3, 4, 5, 6])) == 2.24
 
 def test_calculer_distance_2():
-    assert mh.calculer_distance_2((1, 2, 3, 4, 5), (2, 3, 4, 5, 6)) == 5
+    assert mh.calculer_distance_2(np.array([1, 2, 3, 4, 5]), np.array([2, 3, 4, 5, 6])) == 5
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
