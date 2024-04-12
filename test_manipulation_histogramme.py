12986b48a39e5ec78bbeb308b4bafd923479bb14
LucasParisienG <163561916+LucasParisienG@users.noreply.github.com>
2024-04-12 12:34:30 -0400

    Update test_manipulation_histogramme.py

--- test_manipulation_histogramme.py
+++ test_manipulation_histogramme.py
@@ -1,6 +1,7 @@
 import manipulation_histogramme.py as mh
 import unittest
 import numpy as np
+
 def test_calculer_histogramme():
     assert mh.calculer_histogramme(np.array([225, 160, 10, 49, 20, 170, 1, 121, 30, 223, 230, 100, 255, 23, 155, 88]), 3) == (4, 0, 2, 3, 3, 2, 2, 2, 4, 0, 2, 3, 2, 3, 2, 2)
     
@@ -9,11 +10,6 @@ def test_calculer_distance_1():
 
 def test_calculer_distance_2():
     assert mh.calculer_distance_2((1, 2, 3, 4, 5), (2, 3, 4, 5, 6)) == 5
-
-
-
-
-
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
