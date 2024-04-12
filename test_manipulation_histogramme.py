48004f995520e8520d86b897bda9a05aeda25358
LucasParisienG <163561916+LucasParisienG@users.noreply.github.com>
2024-04-12 12:10:00 -0400

    Update test_manipulation_histogramme.py

--- test_manipulation_histogramme.py
+++ test_manipulation_histogramme.py
@@ -4,7 +4,8 @@ import unittest
 def test_calculer_histogramme()
     assert mh.calculer_histogramme((225, 160, 10, 49, 20, 170, 1, 121, 30, 223, 230, 100, 255, 23, 155, 88), 3) == (4, 0, 2, 3, 3, 2, 2, 2, 4, 0, 2, 3, 2, 3, 2, 2)
     
-
+def test_calculer_distance1()
+    assert mh.calculer_distance1(
 
 
 
